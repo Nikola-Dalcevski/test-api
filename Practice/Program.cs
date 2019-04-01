81b4846268155c6c5c7cac94c9b0c2ad60918a30
Nikola.Dalcevski <46868646+Nikola-Dalcevski@users.noreply.github.com>
2019-04-01 20:46:30 +0200

    Add files via upload

---
+++ Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog jecky = new Dog() { Name = "Jacky", Color = "White", Age = 4 };
+            Dog.Chip chip = new Dog.Chip("Vukasin", "11 Oktomvri");
+            jecky.AddChip(chip);
+            Dog.IdentifyDog(jecky);
+            Console.ReadLine();
+        }
+    }
+}
